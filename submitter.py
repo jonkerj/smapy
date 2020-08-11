9a7938e25ee75db2c01502a242c4795c7acde0d2
Jorik Jonker <jorik@kippendief.biz>
2020-08-11 14:39:25 +0200

    Merge secrets into config

--- submitter.py
+++ submitter.py
@@ -50,7 +50,8 @@ class Submitter(object):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Submit SMA Webconnect to InfluxDB")
-	parser.add_argument("--config", dest='config', help='Config YAML', default='config.yaml')
+	parser.add_argument("--config", dest='config', help='Config YAML', default='/config/config.yaml')
+	parser.add_argument("--secrets", dest='secrets', help='Dir holding secrets', default='/secrets/')
 	parser.add_argument("--debug", dest='level', action='store_const', help='Enable debug logging', const=logging.DEBUG, default=logging.INFO)
 	args = parser.parse_args()
 
@@ -58,6 +59,20 @@ if __name__ == '__main__':
 	with open(args.config, 'r') as f:
 		config = yaml.load(f)
 	
+	assert os.path.exists(args.secrets), f'secrets dir ({args.secrets}) does not exist'
+	assert os.path.isdir(args.secrets), f'secrets path ({args.secrets}) is not a directory'
+
+	# merge secret values (eg: influxdb.connection.username) into config hash
+	for key in os.listdir(args.secrets):
+		if key.startswith('..'):
+			continue
+		logger.debug(f'merging {key}')
+		parts = key.split('.')
+		current = config
+		for k in parts[:-1]:
+			current = current[k]
+		current[parts[-1]] = open(os.path.join(args.secrets, key), 'r').read().strip()
+
 	s = Submitter(config)
 
 	# register exit hook. Needed to not overflow sessions
